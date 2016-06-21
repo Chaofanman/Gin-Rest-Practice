0a207089dd7d5ab115599a010219f77930437435
Chaofanman <gchase.patron@gmail.com>
2016-06-21 17:42:29 +0800

    Reverted and changed layout to include deletion

--- dist/src/Pages/UserList.js
+++ dist/src/Pages/UserList.js
@@ -1,32 +1,13 @@
 import React from 'react';
 
-// const UserList = (props) =>{
-//     return (<div> 
-//             {props.users.map((user, index) => (
-//             	<li key={index}>
-//                 	{user.id}:  {user.Lastname}, {user.Firstname} 
-//                 	<button onClick={event => this.props.onDeleteSubmit({id: user.id})}> X </button>
-//                 </li>
-
-//             ))}
-//         </div>);
-// }
-
-class UserList extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			users: this.props.users
-		}
-		console.log("Users: ", this.state.users);
-	}
-
-	render(){
-		return(<div>
-			<p> ldkfaldf </p>
-		</div>)
-	}
+const UserList = (props) =>{
+    return (<div> 
+            {props.users.map((user, index) => (
+            	<li key={index}>
+                {user.id}: {user.Lastname}, {user.Firstname} <button> X </button>
+                </li>
+            ))}
+        </div>);
 }
-
 
 export default UserList;
