6c111fafe5a3b7b6291061d6a5693b063710e661
Chaofanman <gchase.patron@gmail.com>
2016-06-28 13:41:28 +0800

    Some how it worked

--- dist/src/Pages/User.js
+++ dist/src/Pages/User.js
@@ -1,40 +1,14 @@
 import React from 'react';
 
 const User = ({user}) =>  {
-
 	console.log("In user: ", user)
-
 
 	return(<div>
 		<h1> {user.id} </h1>
-		<h1> {user.firstname} </h1>
-		<h1> {user.lastname} </h1>
+		<h1> {user.Firstname} </h1>
+		<h1> {user.Lastname} </h1>
 	</div>);
 
 }
 
 export default User;
-
-// import React from "react";
-
-// const VideoListItem = ({video, onVideoSelect}) => {
-// 	// const video = props.video;
-// 	const imageUrl = video.snippet.thumbnails.default.url;
-
-// 	return (
-// 		<li onClick = {() =>onVideoSelect(video)}className = "list-group-item">
-// 			<div className = "video-list media">
-// 				<div className = "media-left">
-// 					<img className = "media-object" src = {imageUrl} />
-// 				</div>
-// 				<div className = "media-body">
-// 					<div className = "media-head">
-// 						{video.snippet.title}
-// 					</div>
-// 				</div>
-// 			</div>
-// 		</li>
-// 	);
-// };
-
-// export default VideoListItem;
--- dist/src/Pages/UserList.js
+++ dist/src/Pages/UserList.js
@@ -4,22 +4,6 @@ import { Router, Route, Link, browserHistory } from 'react-router'
 class UserList extends React.Component{
 	constructor(props){
 		super(props);
-		this.handleDelete = this.handleDelete.bind(this);
-		this.giveSingleUser = this.giveSingleUser.bind(this);
-	}
-
-	handleDelete(event){
-		console.log("In handle delete");
-		console.log(event.target.value);
-
-		this.props.onDeleteSubmit(event.target.value);
-	}
-
-	giveSingleUser(event){
-		event.preventDefault();
-		console.log("In giveSingleUser, userid: ", event.target.value);
-
-		this.props.getUser(event.target.value);	
 	}
 
 	render(){
@@ -27,8 +11,8 @@ class UserList extends React.Component{
 			<ul>
 	            {this.props.users.map((user, index) => (
 	            	<li key={index}>
-		                <Link to={`/${user.id}`} value={user.id} onClick={this.giveSingleUser}> {user.id}: {user.Lastname}, {user.Firstname} </Link>
-		                <button onClick={this.handleDelete} value={user.id}> X </button>
+		                <Link to={`/${user.id}`} value={user.id} onClick={event => this.props.getUser(event.target.value)} > {user.id}: {user.Lastname}, {user.Firstname} </Link>
+		                <button onClick={event => this.props.onDeleteSubmit(event.target.value)} value={user.id}> X </button>
 	                </li>
 	            ))}
             </ul>
